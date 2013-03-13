2e429c3205b805f7d52ade8f5cb9cbed3b82b58f
ZHTonyZhang <tony.zhang@quest.com>
2013-03-13 13:40:56 +0800

    qweqweq
    QQQ

--- magicmethon_test.py
+++
@@ -1,8 +0,0 @@
-from os.path  import join
-
-class FileObject:
-	def __init__(self, filepath='~', filename='sample.txt'):
-		self.file = open(join(filepath, filename), 'r+')
-    def __del__(self):
-    	self.file.close()
-    	del self.filen
